064e206b890687c08f0550bee65ce1e31a8b91a4
srikarpavan <47261789+srikarpavan@users.noreply.github.com>
2019-12-05 23:14:36 -0600

    commit

---
+++ source code/WhatsApp/app/src/main/java/com/mys3soft/mys3chat/phonecall.java
@@ -0,0 +1,13 @@
+package com.mys3soft.mys3chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class phonecall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phonecall);
+    }
+}
